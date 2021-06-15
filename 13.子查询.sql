4a98bd7c3179faa8d7c846dbade872b49805404a
shixj@jjyuan.vip <shixj@jjyuan.vip>
2021-06-15 18:04:13 +0800

    13.子查询.sql

--- 13.子查询.sql
+++ 13.子查询.sql
@@ -15,7 +15,7 @@ select first_name from employees where
 		仅支持标量子查询
 	from后面：
 		支持表子查询
-	where后面或having后面：（重点）
+	where后面或having后面： （重点）★
 		标量子查询	（单行）√ 
 		列子查询	（多行）√
 		行子查询
@@ -28,3 +28,78 @@ select first_name from employees where
 	行子查询（结果集有一行多列）
 	表子查询（结果集一般为多行多列）
 */
+
+#一、where或having后面
+/*
+1、标量子查询（单行子查询）
+2、列子查询（多行子查询）
+3、行子查询（多列多行）
+
+特点：
+①子查询放在小括号内
+②子查询一般放在条件的右侧
+③标量子查询，一般搭配单行操作符使用如：   > < >= <= = <>
+列子查询，一般搭配多行操作符号使用
+in、any|some、all
+*/
+
+#1.标量子查询
+
+#案例1：谁的工资比 Abel 高？
+
+#①查询Abel的工资
+SELECT 
+  salary 
+FROM
+  employees 
+WHERE last_name = 'Abel' ;
+
+#②查询员工的信息，满足 salary>①结果
+SELECT 
+  * 
+FROM
+  employees 
+WHERE salary > 
+  (SELECT 
+    salary 
+  FROM
+    employees 
+  WHERE last_name = 'Abel')
+
+#案例2：返回job_id与141号员工相同，salary比143号员工多的员工的姓名、 job_id、 工资
+#①查询141号员工的job_id
+SELECT 
+  job_id
+  FROM `employees`
+  WHERE `employee_id` = 141;
+
+#②查询143号员工的salary
+SELECT salary FROM employees WHERE employee_id = 143;
+
+#③查询员工的姓名、job_id 、 工资，要求 job-id=①并且salary>②
+SELECT 
+  `last_name`,
+  `job_id`,
+  `salary` 
+FROM
+  employees 
+WHERE job_id = 
+  (SELECT 
+    job_id 
+  FROM
+    `employees` 
+  WHERE `employee_id` = 141) 
+  AND salary > 
+  (SELECT 
+    salary 
+  FROM
+    employees 
+  WHERE employee_id = 143) ;
+
+
+
+
+
+
+
+

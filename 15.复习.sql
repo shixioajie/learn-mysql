74ef6c80fa6197c2c0f9ac480cb2981abb705349
shixiaojie <shixj@jjyuan.vip>
2021-07-11 21:01:28 +0800

    15.复习.sql 整理

--- 15.复习.sql
+++ 15.复习.sql
@@ -3,10 +3,12 @@
   
   SQL语法
   
+  一、连接查询
+  
   1、内连接
   select	查询列表
   from		表名1 as 别名1 
-  【inner】 join	表名2 as 别名2
+  【inner】 join	表名2 as 别名2 on 连接条件
   where		分组去过滤条件
   gourp by	分组语句 
   having	分组后过滤
@@ -27,7 +29,7 @@
   
   select	查询列表
   from		表名1 as 别名1 
-  left|right|full 【outer】 join	表名2 as 别名2
+  left|right|full 【outer】 join  表名2 as 别名2  on 连接条件
   where		分组去过滤条件
   gourp by	分组语句 
   having	分组后过滤
@@ -37,7 +39,7 @@
   特点：
    Ⅰ 查询的结果=主表中所有的行，如果从表和它匹配的将显示匹配行，如果从表没有匹配的则显示null。
    Ⅱ left join 左边的就是主表，right join 右边的就是主表。
-   Ⅲ 一般用于查询除了交际部分的剩余的剩余的不匹配的行。
+   Ⅲ 一般用于查询除了交际部分的剩余的不匹配的行。
    
   3、交叉连接
   
@@ -51,7 +53,7 @@
   
   
   
-  2、子查询
+  二、子查询
   
   子查询的意义：
     嵌套再其它语句内部的select语句称为子查询或内查询，
@@ -60,24 +62,70 @@
   
   分类：
     Ⅰ 按出现位置
-    select
-    from
-    where或having后面
-    exists后面
+    select 后面：
+		仅仅支持标量子查询
+    from 后面：
+		表子查询
+    where或having 后面：
+			标量子查询
+			列子查询
+			行子查询
+    exists 后面：
+		标量子查询
+		列子查询
+		行子查询
+		表子查询
     
-    Ⅱ 按结果集的行列
+   Ⅱ 按结果集的行列
     标量子查询（单行子查询）：结果集为一行一列。
     列子查询（多行子查询）：结果集为多行一列。
     行字查询：结果为多行多列。
     表子查询（嵌套子查询）：结果集为多行多列。
+    
+  示例：
+  where 或 having 后面
+  1、标量子查询
+   案例：查询一下最低工资的员工姓名和工资
+   ① 查询最低工资
+   select min(salary) from employees
+   
+   ②查询员工的姓名和工资、要求工资=①
+   select last_name,salary from employees
+   where salary =(select min(salary) from employees)   
+      
+   2、列子查询  
+    案例：查询所有是领导的员工姓名
+    ①查询所有的员工的 manager_id
+    select manager_id from employees 
+    
+    ②查询姓名，employee_id属于①列表的一个
+    select last_name 
+    from employees
+    where employee_id in(
+	select manager_id
+	from employees
+    )
   
   
+  三、分页子查询
+  应用场景：
+	当要查询的条目数太多，一页显示不全
+  语法：
   
-  
-  
-  
-  
-  
+	select 查询列表
+	from	表
+	limit	【offset,】size;
+	
+	注意：offset代表的是起始的条目索引，默认从0开始。
+		size表示的是显示条目数
+		
+	公式：
+	假如要显示的页数为page，每页条目数为size
+	select 查询列表
+	from	表
+	
+	limit (page-1)*size,size;
   
 
 */
+
